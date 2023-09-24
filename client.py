5b4a3be45d2d6f13dee605f4025be2830d0a219f
Mabuo <ma6ko1to6@gmail.com>
2023-09-24 23:40:21 +0000

    stage1の開発 #2

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+
 class Client:
     def __init__(self, server_address='0.0.0.0', server_port=9001):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,10 +12,9 @@ class Client:
     def start(self):
         threading.Thread(target=self.send_messages).start()
         self.receive_messages()
-        
 
     def receive_messages(self):
-        try:    
+        try:
             while True:
                 message, _ = self.client_socket.recvfrom(4096)
                 print(message.decode('utf-8'))
@@ -28,7 +28,8 @@ class Client:
             while True:
                 message_content = input('')
                 message = self.encode_message(self.username, message_content)
-                self.client_socket.sendto(message, (self.server_address, self.server_port))
+                self.client_socket.sendto(
+                    message, (self.server_address, self.server_port))
         finally:
             print('socket closig....')
             self.client_socket.close()
@@ -38,6 +39,7 @@ class Client:
         username_len_byte = len(username_byte).to_bytes(1, byteorder='big')
         message_byte = message_content.encode('utf-8')
         return username_len_byte + username_byte + message_byte
+
 
 if __name__ == "__main__":
     client = Client()
--- server.py
+++ server.py
@@ -2,19 +2,18 @@ import socket
 import threading
 import time
 
+
 class Server:
     def __init__(self, server_address='0.0.0.0', server_port=9001):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind((server_address, server_port))
-        self.clients = {} # アドレスをキーとし、ユーザー名と最後の活動時間を値とする
+        self.clients = {}  # アドレスをキーとし、ユーザー名と最後の活動時間を値とする
         self.TIMEOUT = 60  # クライアントがタイムアウトするまでの秒数
-        
 
     def start(self):
         threading.Thread(target=self.check_client_timeout).start()
         self.receive_messages()
-        
-        
+
     def receive_messages(self):
         try:
             while True:
@@ -22,8 +21,9 @@ class Server:
                 message, address = self.server_socket.recvfrom(4096)
                 username_len = int.from_bytes(message[:1], byteorder='big')
                 username = message[1:1 + username_len].decode('utf-8')
-                self.clients[address] = {'username': username, 'last_time': time.time()}
-                
+                self.clients[address] = {
+                    'username': username, 'last_time': time.time()}
+
                 message_for_send = f"{username}: {message[1 + username_len:].decode('utf-8')}"
                 print(message_for_send)
                 message_for_send = message_for_send.encode('utf-8')
@@ -32,12 +32,10 @@ class Server:
             print('socket closig....')
             self.server_socket.close()
 
-
     def broadcast(self, message, sender_address):
         for address in self.clients:
             self.server_socket.sendto(message, address)
 
- 
     def check_client_timeout(self):
         try:
             while True:
@@ -47,7 +45,8 @@ class Server:
                     if current_time - client_info['last_time'] > self.TIMEOUT:
                         username = client_info['username']
                         print(f"Client {username} ({address}) has timed out.")
-                        timeout_message = f"{username} has timed out and left the chat.".encode('utf-8')
+                        timeout_message = f"{username} has timed out and left the chat.".encode(
+                            'utf-8')
                         self.broadcast(timeout_message, address)
                         inactive_clients.append(address)
                 for address in inactive_clients:
@@ -59,6 +58,6 @@ class Server:
 
 
 if __name__ == "__main__":
-    
+
     server = Server()
     server.start()
