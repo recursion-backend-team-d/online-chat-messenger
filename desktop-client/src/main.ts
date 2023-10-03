5b77122a71ada1a1c5d68df707e9d0b95ca4d061
K4R4 <mabdman512@gmail.com>
2023-10-03 17:08:43 +0900

    Electronの雛形追加 #22

---
+++ desktop-client/src/main.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { BrowserWindow, app } from "electron";
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.resolve(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadFile("dist/index.html");
+  // mainWindow.webContents.openDevTools({ mode: "detach" });
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.once("window-all-closed", () => app.quit());
---
+++ desktop-client/webpack.config.ts
@@ -0,0 +1,70 @@
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const common: Configuration = {
+  mode: isDev ? "development" : "production",
+  externals: ["fsevents"],
+  output: {
+    publicPath: "./",
+    filename: "[name].js",
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(ico|png|svg|eot|woff?2?)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  watch: isDev,
+  devtool: isDev ? "source-map" : undefined,
+};
+
+const main: Configuration = {
+  ...common,
+  target: "electron-main",
+  entry: {
+    main: "./src/main.ts",
+  },
+};
+
+const preload: Configuration = {
+  ...common,
+  target: "electron-preload",
+  entry: {
+    preload: "./src/preload.ts",
+  },
+};
+
+const renderer: Configuration = {
+  ...common,
+  target: "web",
+  entry: {
+    app: "./src/web/index.tsx",
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: "./src/web/index.html",
+    }),
+  ],
+};
+
+export default [main, preload, renderer];
